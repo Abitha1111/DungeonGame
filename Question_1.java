c3591f75ad311a59fafc078fff3977fbb4c8bfc0
Abitha <148053196+Abitha1111@users.noreply.github.com>
2023-12-23 15:23:18 +0530

    Update Question_1.java

--- Question_1.java
+++ Question_1.java
@@ -2,36 +2,52 @@ package DungeonGame;
 
 import java.util.Scanner;
 
-public class Question_1 {
+public class Question_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Dimensions of the dungeon (Row x Column):");
-		int rows = sc.nextInt();
-		int cols = sc.nextInt();
+		System.out.println("Dimensions of the dungeon(Row x Column :  ");
+		int row = sc.nextInt();
+		int col = sc.nextInt();
 
-		System.out.println("Position of adventurer :");
-		int adv1 = sc.nextInt();
-		int adv2 = sc.nextInt();
+		System.out.println("Position of Adventure : ");
+		int advR = sc.nextInt();
+		int advC = sc.nextInt();
 
-		System.out.println("Position of gold :");
-		int gold1 = sc.nextInt();
-		int gold2 = sc.nextInt();
+		System.out.println("Position of Monster : ");
+		int goldR = sc.nextInt();
+		int goldC = sc.nextInt();
 
-		int steps = Math.abs(adv1 - gold1) + Math.abs(adv2 - gold2);
-		System.out.println("Minimum number of steps: " + steps);
-		while (adv1 != gold1 || adv2 != gold2) {
-			if (adv1 < gold1) {
-				adv1++;
-			} else if (adv1 > gold1) {
-				adv1--;
-			} else if (adv2 < gold2) {
-				adv2++;
-			} else if (adv2 > gold2) {
-				adv2--;
-			}
+		System.out.println("Position of Gold : ");
+		int monsR = sc.nextInt();
+		int monsC = sc.nextInt();
+
+		int steps = minimumMoves(row, col, advR, advC, goldR, goldC, monsR, monsC);
+
+		if (steps == -1) {
+			System.out.println("No Possible Solution");
+		} else {
+			System.out.println("Minimum number of steps : " + steps);
 		}
+	}
+
+	private static int minimumMoves(int row, int col, int advR, int advC, int goldR, int goldC, int monsR, int monsC) {
+		int advMoves = Moves(advR, advC, goldR, goldC);
+		int monsterMoves = Moves(monsR, monsC, goldR, goldC);
+
+		if (advMoves <= monsterMoves) {
+			return advMoves;
+		} else {
+			return -1;
+		}
+	}
+
+	private static int Moves(int startR, int startC, int destR, int destC) {
+		int upMove = (startR > destR) ? (startR - destR) : (destR - startR);
+		int downMove = (startC > destC) ? (startC - destC) : (destC - startC);
+
+		return upMove + downMove;
 	}
 }
