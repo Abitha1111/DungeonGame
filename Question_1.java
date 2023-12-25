753cedb91a6642a6ec797ccd7ef5e9893d8c01cc
Abitha <148053196+Abitha1111@users.noreply.github.com>
2023-12-25 19:56:39 +0530

    Update Question_1.java

--- Question_1.java
+++ Question_1.java
@@ -2,89 +2,95 @@ package DungeonGame;
 
 import java.util.*;
 
-public class Question_4 {
+public class Question_5 {
 
-    static class Cell {
-        int row, col;
+	static class Cell {
+		int row, col;
 
-        Cell(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
+		Cell(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        System.out.print("Dimensions of the dungeon (Row x Column): ");
-        int rows = sc.nextInt();
-        int cols = sc.nextInt();
+		System.out.print("Dimensions of the dungeon (Row x Column): ");
+		int rows = sc.nextInt();
+		int cols = sc.nextInt();
 
-        System.out.print("Position of adventurer: ");
-        int advR = sc.nextInt();
-        int advC = sc.nextInt();
+		System.out.print("Position of adventurer: ");
+		int advR = sc.nextInt();
+		int advC = sc.nextInt();
 
-        System.out.print("Position of monster: ");
-        int monsR = sc.nextInt();
-        int monsC = sc.nextInt();
+		System.out.print("Position of gold: ");
+		int goldR = sc.nextInt();
+		int goldC = sc.nextInt();
 
-        System.out.print("Position of trigger: ");
-        int trigR = sc.nextInt();
-        int trigC = sc.nextInt();
+		System.out.print("Enter number of pits: ");
+		int numPits = sc.nextInt();
 
-        System.out.print("Position of gold: ");
-        int goldR = sc.nextInt();
-        int goldC = sc.nextInt();
+		Set<Cell> pits = new HashSet<>();
+		System.out.println("Position of pits:");
+		for (int i = 0; i < numPits; i++) {
+			System.out.print("Position of pit " + (i + 1) + ": ");
+			int pitR = sc.nextInt();
+			int pitC = sc.nextInt();
+			pits.add(new Cell(pitR, pitC));
+		}
 
-        int minSteps = findMinimumSteps(rows, cols, advR, advC, monsR, monsC, trigR, trigC, goldR, goldC);
+		int minSteps = findMinimumSteps(rows, cols, advR, advC, goldR, goldC, pits);
 
-        System.out.println("Minimum number of steps: " + minSteps);
-    }
+		if (minSteps != -1) {
+			System.out.println("Minimum number of steps: " + minSteps);
+		} else {
+			System.out.println("No possible solution");
+		}
+	}
 
-    static int findMinimumSteps(int rows, int cols, int advR, int advC, int monsR, int monsC, int trigR, int trigC, int goldR, int goldC) {
-        boolean[][] visited = new boolean[rows][cols];
-        Queue<Cell> queue = new LinkedList<>();
+	static int findMinimumSteps(int rows, int cols, int advR, int advC, int goldR, int goldC, Set<Cell> pits) {
+		boolean[][] visited = new boolean[rows][cols];
+		Queue<Cell> queue = new LinkedList<>();
 
-        Cell adventurer = new Cell(advR, advC);
-        Cell monster = new Cell(monsR, monsC);
-        Cell trigger = new Cell(trigR, trigC);
-        Cell gold = new Cell(goldR, goldC);
+		Cell adventurer = new Cell(advR, advC);
+		Cell gold = new Cell(goldR, goldC);
 
-        int[][] directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        queue.add(adventurer);
-        visited[adventurer.row - 1][adventurer.col - 1] = true;
+		int[][] directions = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+		queue.add(adventurer);
+		visited[adventurer.row - 1][adventurer.col - 1] = true;
 
-        int steps = 0;
+		int steps = 0;
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+		while (!queue.isEmpty()) {
+			int size = queue.size();
 
-            while (size-- > 0) {
-                Cell current = queue.poll();
+			while (size-- > 0) {
+				Cell current = queue.poll();
 
-                if (current.row == gold.row && current.col == gold.col) {
-                    return steps;
-                }
+				if (current.row == gold.row && current.col == gold.col) {
+					return steps;
+				}
 
-                for (int[] dir : directions) {
-                    int newRow = current.row + dir[0];
-                    int newCol = current.col + dir[1];
+				for (int[] dir : directions) {
+					int newRow = current.row + dir[0];
+					int newCol = current.col + dir[1];
 
-                    Cell newCell = new Cell(newRow, newCol);
+					Cell newCell = new Cell(newRow, newCol);
 
-                    if (isValidMove(rows, cols, newCell, visited, monster, trigger)) {
-                        queue.add(newCell);
-                        visited[newRow - 1][newCol - 1] = true;
-                    }
-                }
-            }
-            steps++;
-        }
-        return -1; 
-    }
+					if (isValidMove(rows, cols, newCell, visited, pits)) {
+						queue.add(newCell);
+						visited[newRow - 1][newCol - 1] = true;
+					}
+				}
+			}
+			steps++;
+		}
+		return -1; 
+	}
 
-    static boolean isValidMove(int rows, int cols, Cell cell, boolean[][] visited, Cell monster, Cell trigger) {
-        return cell.row >= 1 && cell.row <= rows && cell.col >= 1 && cell.col <= cols
-                && !visited[cell.row - 1][cell.col - 1] && !cell.equals(monster) && !cell.equals(trigger);
-    }
+	static boolean isValidMove(int rows, int cols, Cell cell, boolean[][] visited, Set<Cell> pits) {
+		return cell.row >= 1 && cell.row <= rows && cell.col >= 1 && cell.col <= cols
+				&& !visited[cell.row - 1][cell.col - 1] && !pits.contains(cell);
+	}
 }
